1c86bf111fff4c42ed80e2ccf0cc609837ef9ff5
danicontec <daniel.mrt.bkt@gmail.com>
2023-06-02 06:35:38 +0200

    Empezando estructura de ejemplo de subida de archivos a servidor

---
+++ MANEJO-IMAGENES/formulario-subida.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Busqueda de imagenes</h2>
+    <form method="POST" enctype="multipart/form-data" action="process.php">
+    <label for="imagen">Buscar</label>
+    <input type="file" name="imagen" id="imagen">
+    <input type="submit" value="Subir">
+    </form>
+    <?php 
+
+    ?>
+</body>
+</html> 
---
+++ MANEJO-IMAGENES/process.php
@@ -0,0 +1,32 @@
+<?php
+
+    /* Aqui se procesara todos los datos de la imagen, a traves de la variable 
+    superglobal $_FILE, se le puede indicar un nombre, tamaño y una ubicacion en el
+    servidor, como todas las variables superglobales trabajan con arrays para guardar
+    estas propiedades, pero antes de eso crea una carpeta temporal*/
+
+    $img_name = $_FILES["imagen"]["name"];
+    $type_img = $_FILES["imagen"]["type"];
+    $size_img = $_FILES["imagen"]["size"];
+    
+    //DOCUMENT_ROOT indica la ruta del servidor desde la raiz (www en caso de Wamp)
+    $send_directory = $_SERVER["DOCUMENT_ROOT"] . "/img/uploads/";
+
+    //Si el directorio no existe en la ruta especificada se crea, si ya existe, solo sube el archivo
+    if(!file_exists($send_directory)){
+
+        /*Primero ruta, despues permisos (parecido a Linux), por ultimo especificar
+        si es carpeta recursiva, es decir si son anidadas con valor boolean*/
+        mkdir($send_directory, 0777, true);
+    } 
+
+    if(file_exists($send_directory)){
+
+        //Este método mueve la imagen subida del directorio temporal a la ruta especificada
+        move_uploaded_file($_FILES["imagen"]["tmp_name"], $send_directory . $img_name);
+        echo "Archivo subido al servidor";
+    }
+
+    
+
+?>
